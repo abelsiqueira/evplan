e27c594e9116c417cf50050a0b1a6aa74794ddad
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-07-31 10:50:19 -0300

    Add creation of empty schedule

--- evplan/event.py
+++ evplan/event.py
@@ -2,6 +2,7 @@
 These are related to the event
 """
 
+import datetime
 import os
 import yaml
 
@@ -25,18 +26,42 @@ class Event(object):
         self.location = data['location']
         self.duration = data['duration']
         self.times = data['times']
-        t = int(data['interval'])
-        self.schedule = []
+        self.interval = int(data['interval'])
+        self.date_format = data['date_format']
+        days = []
+        p1day = datetime.timedelta(days=1)
+        for date in self.duration.split(','):
+            date = date.split('-')
+            if len(date) == 1:
+                date = to_date(date[0])
+                days.append(date)
+            elif len(date) == 2:
+                date[0] = to_date(date[0])
+                date[1] = to_date(date[1])
+                if date[0] >= date[1]:
+                    print("ERROR: Badly formated date")
+                while (date[0] < date[1]):
+                    days.append(date[0])
+                    date[0] += p1day
+        days = [self.format_date(day) for day in days]
+        times = []
         for pair in self.times.split(','):
             a = time_str_to_int(pair.split('-')[0])
             b = time_str_to_int(pair.split('-')[1])
             while a < b:
-                self.schedule.append(time_int_to_str(a))
-                a += t
+                times.append(time_int_to_str(a))
+                a += self.interval
             if a > b:
                 print("ERROR: Time interval not consistent with schedule")
                 exit(1)
+        self.days = days;
+        self.times = times;
+        self.schedule = dict([(day, dict([(time,'') for time in times]))\
+                for day in days])
         print(self.schedule)
+
+    def format_date(self, s):
+        return s.strftime(self.date_format)
 
 def time_str_to_int(s):
     v = s.split('h')
@@ -50,3 +75,8 @@ def time_str_to_int(s):
 
 def time_int_to_str(v):
     return '{}h{:0>2}'.format(int(v/60),v%60)
+
+def to_date(s):
+    v = s.split('/')
+    return datetime.date(int(v[0]),int(v[1]),int(v[2]))
+
